26c5058e7765437bb76892a4b0ab9566df696d3d
Oswaldo1002009 <A01206725@itesm.mx>
2019-02-07 17:00:28 -0600

    Finished Lab1

--- App1/app/src/main/java/com/example/app1/MainActivity.java
+++ App1/app/src/main/java/com/example/app1/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.app1;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
     }
 
     //Button methods
     public void Next(View view){
         Intent next = new Intent(this, Main2Activity.class);
+        next.putExtra("data", editText.getText().toString());
         startActivity(next);
     }
+
+
 }
